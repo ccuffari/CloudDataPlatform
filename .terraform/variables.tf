9c51fa632ea0bf196f05ee69441458bc7e0082da
ccuffari <75679158+ccuffari@users.noreply.github.com>
2025-01-23 17:45:03 +0100

    Update variables.tf

--- .terraform/variables.tf
+++ .terraform/variables.tf
@@ -20,12 +20,6 @@ variable "location" {
   default     = "europe-north1"
 }
 
-
-variable "project" {
-  description = "GCP project ID"
-  type        = string
-}
-
 variable "service_accounts" {
   description = "Mappa dei service account da creare, con i dettagli del display_name e della descrizione"
   type = map(object({
